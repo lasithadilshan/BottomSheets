fa2a1f4085d016a7b78b99c996e5e040f5d70b41
Lasitha Dilshan <dilshantilakaratne29@gmail.com>
2021-04-09 23:33:36 +0530

    Initial commit

---
+++ app/src/main/java/com/dilshan/bottomsheets/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dilshan.bottomsheets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.order);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+                bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+
+            }
+        });
+    }
+}
